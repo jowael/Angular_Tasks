400d5c90b9cd474f539a6effb0589d0efbf10fa0
jowael <yousefwail22@gmail.com>
2026-02-23 03:11:25 +0200

    feat: initialize Angular project with basic structure and components
    - Added package.json with Angular dependencies and scripts.
    - Included favicon.ico for the application.
    - Created app configuration file for routing and error handling.
    - Developed main app component with child components.
    - Implemented child components (Child1 and Child2) with respective templates and styles.
    - Set up global styles and main HTML structure.
    - Configured TypeScript settings for the application and tests.

---
+++ day3_study/src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Child1 } from './components/child1/child1';
+import { Child2 } from './components/child2/child2';
+
+@Component({
+  selector: 'app-root',
+  imports: [Child1, Child2],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {}
